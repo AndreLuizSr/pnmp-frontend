177024b042dad7ac2de2e4e48c93cc509139406a
JVHabel <jvhabelspi@hotmail.com>
2024-07-04 14:26:25 -0300

    logout user inactivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 import React, { Suspense } from 'react';
-import { useRoutes } from 'react-router-dom';
+import { useState , useEffect } from 'react';
+import { useRoutes, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Themeroutes from './routes/Router';
 import ThemeSelector from './layouts/theme/ThemeSelector';
 import Loader from './layouts/loader/Loader';
+
+
 function App() {
   const routing = useRoutes(Themeroutes);
   const direction = useSelector((state) => state.customizer.isRTL);
   const isMode = useSelector((state) => state.customizer.isDark);
+
+  const navigate = useNavigate();
+  let logoutTimer;
+  const INACTIVITY_TIME_LIMIT = 15 * 60 * 100; // 15 min
+
+  const resetLogoutTimer = () => {
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
+    logoutTimer = setTimeout(handleLogout, INACTIVITY_TIME_LIMIT);
+  };
+
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');
+    navigate('/auth/login');
+  };
+
+  useEffect(() => {
+    window.addEventListener('mousemove', resetLogoutTimer);
+    window.addEventListener('keydown', resetLogoutTimer);
+    resetLogoutTimer();
+    return () => {
+      window.removeEventListener('mousemove', resetLogoutTimer);
+      window.removeEventListener('keydown', resetLogoutTimer);
+    };
+  }, []);
+
   return (
     <Suspense fallback={<Loader />}>
       <div
